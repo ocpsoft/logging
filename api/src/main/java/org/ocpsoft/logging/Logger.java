c53dfb863560a8f7b406f43f58bf58b37e87f2bf
Christian Kaltepoth <christian@kaltepoth.de>
2016-07-20 07:01:05 +0200

    Prevent log event creation of disabled levels

--- api/src/main/java/org/ocpsoft/logging/Logger.java
+++ api/src/main/java/org/ocpsoft/logging/Logger.java
@@ -33,27 +33,42 @@ public abstract class Logger
 
    public void trace(final String msg)
    {
-      log(Level.TRACE, msg, null);
+      if (isTraceEnabled())
+      {
+         log(Level.TRACE, msg, null);
+      }
    }
 
    public void trace(final String msg, final Object arg)
    {
-      log(Level.TRACE, format(msg, new Object[] { arg }), null);
+      if (isTraceEnabled())
+      {
+         log(Level.TRACE, format(msg, new Object[] { arg }), null);
+      }
    }
 
    public void trace(final String msg, final Object arg1, final Object arg2)
    {
-      log(Level.TRACE, format(msg, new Object[] { arg1, arg2 }), null);
+      if (isTraceEnabled())
+      {
+         log(Level.TRACE, format(msg, new Object[] { arg1, arg2 }), null);
+      }
    }
 
    public void trace(final String msg, final Object[] argArray)
    {
-      log(Level.TRACE, format(msg, argArray), null);
+      if (isTraceEnabled())
+      {
+         log(Level.TRACE, format(msg, argArray), null);
+      }
    }
 
    public void trace(final String msg, final Throwable t)
    {
-      log(Level.TRACE, msg, t);
+      if (isTraceEnabled())
+      {
+         log(Level.TRACE, msg, t);
+      }
    }
 
    public boolean isDebugEnabled()
@@ -63,27 +78,42 @@ public abstract class Logger
 
    public void debug(final String msg)
    {
-      log(Level.DEBUG, msg, null);
+      if (isDebugEnabled())
+      {
+         log(Level.DEBUG, msg, null);
+      }
    }
 
    public void debug(final String msg, final Object arg)
    {
-      log(Level.DEBUG, format(msg, new Object[] { arg }), null);
+      if (isDebugEnabled())
+      {
+         log(Level.DEBUG, format(msg, new Object[] { arg }), null);
+      }
    }
 
    public void debug(final String msg, final Object arg1, final Object arg2)
    {
-      log(Level.DEBUG, format(msg, new Object[] { arg1, arg2 }), null);
+      if (isDebugEnabled())
+      {
+         log(Level.DEBUG, format(msg, new Object[] { arg1, arg2 }), null);
+      }
    }
 
    public void debug(final String msg, final Object[] argArray)
    {
-      log(Level.DEBUG, format(msg, argArray), null);
+      if (isDebugEnabled())
+      {
+         log(Level.DEBUG, format(msg, argArray), null);
+      }
    }
 
    public void debug(final String msg, final Throwable t)
    {
-      log(Level.DEBUG, msg, t);
+      if (isDebugEnabled())
+      {
+         log(Level.DEBUG, msg, t);
+      }
    }
 
    public boolean isInfoEnabled()
@@ -93,27 +123,42 @@ public abstract class Logger
 
    public void info(final String msg)
    {
-      log(Level.INFO, msg, null);
+      if (isInfoEnabled())
+      {
+         log(Level.INFO, msg, null);
+      }
    }
 
    public void info(final String msg, final Object arg)
    {
-      log(Level.INFO, format(msg, new Object[] { arg }), null);
+      if (isInfoEnabled())
+      {
+         log(Level.INFO, format(msg, new Object[] { arg }), null);
+      }
    }
 
    public void info(final String msg, final Object arg1, final Object arg2)
    {
-      log(Level.INFO, format(msg, new Object[] { arg1, arg2 }), null);
+      if (isInfoEnabled())
+      {
+         log(Level.INFO, format(msg, new Object[] { arg1, arg2 }), null);
+      }
    }
 
    public void info(final String msg, final Object[] argArray)
    {
-      log(Level.INFO, format(msg, argArray), null);
+      if (isInfoEnabled())
+      {
+         log(Level.INFO, format(msg, argArray), null);
+      }
    }
 
    public void info(final String msg, final Throwable t)
    {
-      log(Level.INFO, msg, t);
+      if (isInfoEnabled())
+      {
+         log(Level.INFO, msg, t);
+      }
    }
 
    public boolean isWarnEnabled()
@@ -123,27 +168,42 @@ public abstract class Logger
 
    public void warn(final String msg)
    {
-      log(Level.WARN, msg, null);
+      if (isWarnEnabled())
+      {
+         log(Level.WARN, msg, null);
+      }
    }
 
    public void warn(final String msg, final Object arg)
    {
-      log(Level.WARN, format(msg, new Object[] { arg }), null);
+      if (isWarnEnabled())
+      {
+         log(Level.WARN, format(msg, new Object[] { arg }), null);
+      }
    }
 
    public void warn(final String msg, final Object arg1, final Object arg2)
    {
-      log(Level.WARN, format(msg, new Object[] { arg1, arg2 }), null);
+      if (isWarnEnabled())
+      {
+         log(Level.WARN, format(msg, new Object[] { arg1, arg2 }), null);
+      }
    }
 
    public void warn(final String msg, final Object[] argArray)
    {
-      log(Level.WARN, format(msg, argArray), null);
+      if (isWarnEnabled())
+      {
+         log(Level.WARN, format(msg, argArray), null);
+      }
    }
 
    public void warn(final String msg, final Throwable t)
    {
-      log(Level.WARN, msg, t);
+      if (isWarnEnabled())
+      {
+         log(Level.WARN, msg, t);
+      }
    }
 
    public boolean isErrorEnabled()
@@ -153,27 +213,42 @@ public abstract class Logger
 
    public void error(final String msg)
    {
-      log(Level.ERROR, msg, null);
+      if (isErrorEnabled())
+      {
+         log(Level.ERROR, msg, null);
+      }
    }
 
    public void error(final String msg, final Object arg)
    {
-      log(Level.ERROR, format(msg, new Object[] { arg }), null);
+      if (isErrorEnabled())
+      {
+         log(Level.ERROR, format(msg, new Object[] { arg }), null);
+      }
    }
 
    public void error(final String msg, final Object arg1, final Object arg2)
    {
-      log(Level.ERROR, format(msg, new Object[] { arg1, arg2 }), null);
+      if (isErrorEnabled())
+      {
+         log(Level.ERROR, format(msg, new Object[] { arg1, arg2 }), null);
+      }
    }
 
    public void error(final String msg, final Object[] argArray)
    {
-      log(Level.ERROR, format(msg, argArray), null);
+      if (isErrorEnabled())
+      {
+         log(Level.ERROR, format(msg, argArray), null);
+      }
    }
 
    public void error(final String msg, final Throwable t)
    {
-      log(Level.ERROR, msg, t);
+      if (isErrorEnabled())
+      {
+         log(Level.ERROR, msg, t);
+      }
    }
 
    protected String format(final String msg, final Object[] args)
